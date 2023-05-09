8593795b478dd63272c1548dd23c42c6d5cfbacf
MakenaG <makenaglory44@gmail.com>
2023-05-09 20:14:13 +0300

    updated projects page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,30 +2,29 @@ import React from "react";
 
 function About() {
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="container mx-auto px-4 py-8 bg-gradient-to-r from-purple-400 to-blue-500">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
         <div className="flex items-center justify-center">
           <img
             src="https://via.placeholder.com/300x300"
             alt="Profile"
-            className="w-64 h-64 rounded-full"
+            className="w-64 h-64 rounded-full border-4 border-white shadow-lg"
           />
         </div>
-        <div>
-          <h2 className="text-3xl font-bold text-gray-800">About me</h2>
-          <p className="text-gray-600 mt-4">
+        <div className="text-white">
+          <h2 className="text-4xl font-bold">About me</h2>
+          <p className="text-lg mt-4">
             I'm a passionate and curious developer who loves to learn new
             technologies and create amazing web applications. I have a bachelor's
-            degree in computer science and I'm currently working as a senior
-            developer at ABC Inc.
+            degree in computer science. 
           </p>
-          <p className="text-gray-600 mt-4">
+          <p className="text-lg mt-4">
             I enjoy solving challenging problems and collaborating with other
             developers. I'm always looking for new opportunities to grow and
             improve my skills.
           </p>
-          <h3 className="text-2xl font-bold text-gray-800 mt-8">Skills</h3>
-          <ul className="list-disc list-inside text-gray-600 mt-4">
+          <h3 className="text-3xl font-bold mt-8">Skills</h3>
+          <ul className="list-disc list-inside text-lg mt-4 space-y-2">
             <li>React</li>
             <li>Node.js</li>
             <li>Ruby</li>
